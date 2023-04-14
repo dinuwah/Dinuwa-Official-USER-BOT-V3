c95c7f2b093b1784b02ec69e2f7112c1f254a2f2
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-04-14 13:18:31 +0530

    💝 Dinuwa Official²⁰²³ 💝

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -9,35 +9,35 @@ try {
   let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
   const url = 'https://www.youtube.com/watch?v=' + videoId
   let whmodsdev = `*${htki} PLAY ${htka}*
-  📌 *Title:* ${title}
-🔗 *Url:* ${url}
-📔 *Description:* ${description}
-⏲️ *Published:* ${publishedTime}
-⌚ *Duration:* ${durationH}
-👀 *Views:* ${viewH}
+  🇱🇰 *Title:* ${title}
+💝 *Url:* ${url}
+💝 *Description:* ${description}
+💝 *Published:* ${publishedTime}
+💝 *Duration:* ${durationH}
+💝 *Views:* ${viewH}
   `
   await conn.sendButton(m.chat, whmodsdev, wm, botdate, [
-    ['🎶 Audio', `${usedPrefix}yta ${url} yes`],
-    ['🎥 Video', `${usedPrefix}ytv ${url} yes`],
-    ['🔎 Youtube Search', `${usedPrefix}yts ${text}`]
+    ['🇱🇰 Audio', `${usedPrefix}yta ${url} yes`],
+    ['🇱🇰 Video', `${usedPrefix}ytv ${url} yes`],
+    ['🇱🇰 Youtube Search', `${usedPrefix}yts ${text}`]
 ], m, fdoc)
 } catch {
-if (!text) throw `Use example ${usedPrefix}${command} arcade`
+if (!text) throw `Use example ${usedPrefix}${command} 💝 Dinuwa Official²⁰²³ 💝`
   let vid = (await youtubeSearch(text)).video[0]
   if (!vid) throw 'Video/Audio Tidak Ditemukan'
   let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
   let url = 'https://www.youtube.com/watch?v=' + videoId
   let ytLink = `https://yt-downloader.akkun3704.repl.co/?url=${url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
   let capt = `*${htki} PLAY ${htka}*
-  📌 *Title:* ${title}
-🔗 *Url:* ${url}
-📔 *Description:* ${description}
-⏲️ *Published:* ${publishedTime}
-⌚ *Duration:* ${durationH}
-👀 *Views:* ${viewH}
+  🇱🇰 *Title:* ${title}
+💝 *Url:* ${url}
+💝 *Description:* ${description}
+💝 *Published:* ${publishedTime}
+💝 *Duration:* ${durationH}
+💝 *Views:* ${viewH}
   `
-  let buttons = [{ buttonText: { displayText: '🎶 Audio' }, buttonId: `${usedPrefix}opta ${url}` }, { buttonText: { displayText: '🎥 Video' }, buttonId: `${usedPrefix}optv ${url}` }]
-  let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: 'Follow me on instagram:\n\tinstagram.com/itx_ahmad.ali', buttons }, { quoted: m })
+  let buttons = [{ buttonText: { displayText: '🇱🇰 Audio' }, buttonId: `${usedPrefix}opta ${url}` }, { buttonText: { displayText: '🇱🇰 Video' }, buttonId: `${usedPrefix}optv ${url}` }]
+  let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: 'Follow me on Github:\n\tgithub.com/dinuwah/Dinuwa-Official-USER-BOT-V3', buttons }, { quoted: m })
   // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
 } 
 }
