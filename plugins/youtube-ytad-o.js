bd8b16fd9701cd0ecacb22cde6b73bcd6c64c8c7
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-04-14 13:46:19 +0530

    💖 Dinuwa Official²⁰²³ 💖

--- plugins/youtube-ytad-o.js
+++ plugins/youtube-ytad-o.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }
  let capt = `*${htki} YOUTUBE ${htka}*
  _Please select the type below_
  `
-let buttons = [{ buttonText: { displayText: 'Audio' }, buttonId: `${usedPrefix}yta ${text} ` }, { buttonText: { displayText: 'Document' }, buttonId: `${usedPrefix}adoc ${text} ` }]
+let buttons = [{ buttonText: { displayText: '💖 Audio 💖' }, buttonId: `${usedPrefix}yta ${text} ` }, { buttonText: { displayText: '💖 Document 💖' }, buttonId: `${usedPrefix}adoc ${text} ` }]
 							 
-let msg = await conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/cc8jfkX/01.jpg' }, caption: capt, footer: '_*Whatsapp bot by AHMAD ALI*_', buttons }, { quoted: m })
+let msg = await conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' }, caption: capt, footer: '_*Whatsapp bot by 💖 Dinuwa Official²⁰²³ 💖*_', buttons }, { quoted: m })
 }
 
 handler.command = /^opta?$/i
