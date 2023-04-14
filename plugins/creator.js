5ab03f76026f35dcf1001a0022f66f697b4e85f1
dinuwah <119771445+dinuwah@users.noreply.github.com>
2023-04-14 12:42:55 +0530

    💝 Dinuwa Official²⁰²³ 💝

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,10 +5,10 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `ɪɴɴᴏᴄᴇɴᴛ ᴍᴏʀᴇ ᴛʜᴀɴ ɢᴏᴏɢʟᴇ`, `ikratosofc@gmail.com`, `🇵🇰 Pakistan`, `🐙 https://github.com/xIKRATOSx`, `👑 ᴏᴡɴᴇʀ ɪᴋʀᴀᴛᴏs ʙᴏᴛ`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `📵 ᴅᴏɴᴛ sᴘᴀᴍ/ᴄᴀʟʟ ᴍᴇ 😢`, `ɴᴏᴛʜɪɴɢ`, `🇵🇰 Pakistan`, `🐙 https://github.com/xIKRATOSx/Ikratos-MD-v1`, `🤖 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `ɪɴɴᴏᴄᴇɴᴛ ᴍᴏʀᴇ ᴛʜᴀɴ ɢᴏᴏɢʟᴇ`, `Dinuwa-Official-USER-BOT-V3@gmail.com`, `Sri Lanka`, `💝  https://github.com/dinuwah`, `💝  ᴏᴡɴᴇʀ 💝 Dinuwa Official²⁰²³ 💝`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `💝 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `💝 ᴅᴏɴᴛ sᴘᴀᴍ/ᴄᴀʟʟ ᴍᴇ 💝`, `ɴᴏᴛʜɪɴɢ`, `Sri Lanka`, `💝 https://github.com/dinuwah/Dinuwa-Official-USER-BOT-V3`, `💝 Dinuwa-Official-USER-BOT-V3`]
   ], fkontak)
-  await m.reply(`Listen baby @${m.sender.split('@')[0]}, That's My Owner Don't be over, Okay? 😃\nTry to be polite\nDon\t call him or you will be ignored`)
+  await m.reply(`Hi cuddh mk itim @${m.sender.split('@')[0]}, මූ තමා මගේ owner, 😃\nඕන වුනොත් contact කරපන් වට්සැප්`)
   } 
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
